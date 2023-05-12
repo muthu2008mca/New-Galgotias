8e7f8f3f8a55fa028b03e83da3fa1ee266354def
muthu2008mca <muthu2008mca@gmail.com>
2023-05-12 11:18:18 +0530

    rest-change

---
+++ session2/RestOperator.js
@@ -0,0 +1,9 @@
+// REST is used for packing elements into array(...x)
+function sum(...x)
+{
+   console.log(x)
+}
+
+// sum(5,10,23,2,5,67,7,8,98,9,5,6,7,8,8,8,7)
+
+sum(5,10,"Rahul",{am:20},67,7,8,98,9,5,6,7,8,8,8,7)
---
+++ session2/Restructuring.js
@@ -0,0 +1,3 @@
+//Restructuring the data into one object
+let dataobj = {name,duration,version,training}
+console.log(dataobj)
