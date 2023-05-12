4419a504bf52f87ab2f7596edba201217cd3ecfb
muthu2008mca <muthu2008mca@gmail.com>
2023-05-12 11:00:43 +0530

    change4

--- session2/ArrowFunction.js
+++ session2/ArrowFunction.js
@@ -15,3 +15,20 @@ function add(x,y){
 
 let addition = x => x;
 console.log(addition(5))
+
+
+// ========================================
+var names = ["john", "Peter","parker"]
+// let len = names.map((i)=>i.length)
+let len = names.map((i)=>{
+     return i.length
+})
+console.log(len)
+
+// let len = names.forEach((i)=>{
+//     return i.length
+// })
+
+names.forEach((i)=>{
+        console.log(i.length)
+    })
