07f415a63441b0bb0ca047615aeececa8e227417
muthu2008mca <muthu2008mca@gmail.com>
2023-05-12 12:11:59 +0530

    new code

---
+++ session2/class-dom.js
@@ -0,0 +1,14 @@
+let btn = document.querySelector('button');
+
+btn.onclick = function(e){
+    e.preventDefault()
+    console.log("button clicked")
+    console.log("function callled")
+
+    name = document.getElementById('name').values
+    console.log(name)
+    age = document.getElementById('name').values
+    console.log(age)
+    salary = document.getElementById('name').values
+    console.log(salary)
+}
