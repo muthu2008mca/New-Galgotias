a74d007a7d8d22b967907ff977d15582d507a18d
muthu2008mca <muthu2008mca@gmail.com>
2023-05-12 10:45:21 +0530

    cahnge3

---
+++ session2/ArrowFunction.js
@@ -0,0 +1,17 @@
+function add(x,y){
+    return x+y
+}
+
+// let addition = function(x,y){
+//     return x+y
+// };
+
+// let addition = (x,y) => {
+//     return x + y
+
+// }
+
+// let addition = (x,y) => x + y;
+
+let addition = x => x;
+console.log(addition(5))
