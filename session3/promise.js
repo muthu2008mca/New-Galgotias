df2eb22adff2c18cbeb21dcd1c0ba0c89380a4eb
muthu2008mca <muthu2008mca@gmail.com>
2023-05-15 11:41:05 +0530

    today changes

--- session3/promise.js
+++ session3/promise.js
@@ -1,6 +1,21 @@
-//promise : It is used for ES6
-//promise is an object, that return a value that hope to recieve it feature
-//promise is well suited for Asynchronous programming
-//promise is a way of communication with API
+//Promise : It is used for ES6
+//Promise is an object, that return a value that hope to recieve it feature
+//Promise is well suited for Asynchronous programming
+//Promise is a way of communication with API
 //Promise has tree states: Pending, Fullfilled, and Rejected
 
+var sayHi = () => {
+    return new Promise((resolve,reject)=>{
+        let res = true
+        if(res)
+        {
+            resolve("Yes I have a meeting with muthu sir")
+        }
+        else
+        {
+           reject("I Failed ny pormise")
+        }
+    });
+}
+//console.log(sayHi());
+sayHi().then(res=>console.log(res)).catch(err=>console.log(err))
